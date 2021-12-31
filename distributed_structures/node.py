fd2989364bb55fda9d69187596470910c3b9caa6
DylanEHolland <salinson1138@gmail.com>
2021-12-31 12:44:48 -0600

    Using pdm

--- distributed_structures/node.py
+++ distributed_structures/node.py
@@ -3,9 +3,14 @@ from os.path import exists
 
 class node:
     data_dir = f"{environ.get('HOME')}/dd_node"
+    
     def __init__(self):
+        self.peers_dir = f"{self.data_dir}/peers"
         if not exists(self.data_dir):
             mkdir(self.data_dir)
-    
+
+        if not exists(self.peers_dir):
+            mkdir(self.peers_dir)
+            
     def list(self, name):
         pass
--- tests/connect.py
+++ tests/connect.py
@@ -1,4 +1,4 @@
 from distributed_structures import node
 
 n = node()
-my_list = n.list("my_list")
+# my_list = n.list("my_list")
